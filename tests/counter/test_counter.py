91716f613ff2d425dc32226619363a68f0bf8f9a
Italo <contatoitallo@hotmail.com>
2022-10-24 06:40:22 -0300

    chore: :construction: Implementa parcialmente um teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,17 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    """A função retorna corretamente a quantidade de ocorrências da palavra
+    javascript desprezando estrutura de letra maiúsculas e minúsculas"""
+    result_lowercase = count_ocurrences("src/jobs.csv", "javascript")
+    result_uppercase = count_ocurrences("src/jobs.csv", "Javascript")
+
+    assert result_uppercase == result_lowercase
+
+    """A função retorna corretamente a quantidade de ocorrências da palavra
+    python desprezando estrutura de letra maiúsculas e minúsculas"""
+    result_lowercase = count_ocurrences("src/jobs.csv", "python")
+    result_uppercase = count_ocurrences("src/jobs.csv", "Python")
+
+    assert result_uppercase == result_lowercase
