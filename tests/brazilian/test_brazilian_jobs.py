7b1de5287a1d63f1cccc3ef702723b9adad4247d
Italo9 <contatoitallo@hotmail.com>
2024-12-02 14:33:37 -0300

    update: atualiza giot com local para projeto de análises a partir de um conjunto de dados sobre empregos

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result_job = read_brazilian_file("src/jobs.csv")
+    for job in result_job:
+        assert (
+            (job.keys() is "titulo")
+            and (job["salario"] is True)
+            and (job["tipo"] is True)
+        )
